f226575b565b77d75742548190c58a793845b653
DevFerreiraG <Contato.FerreiraG@outlook.com>
2025-04-20 13:15:34 -0300

    sua mensagem

--- server.js
+++ server.js
@@ -4,11 +4,9 @@
  * ***************************************************/
  
 const myApp = require('./myApp');
-const express = require('express');
-const app = express();
 
 if (!process.env.DISABLE_XORIGIN) {
-  app.use((req, res, next) => {
+  myApp.use((req, res, next) => {
     const allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
     const origin = req.headers.origin || '*';
     if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
@@ -21,7 +19,7 @@ if (!process.env.DISABLE_XORIGIN) {
 }
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+myApp.listen(port, () => {
   console.log(`Node is listening on port ${port}...`);
 });
 
