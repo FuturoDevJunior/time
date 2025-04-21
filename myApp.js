9f6c62d6f309bb3ab5803e74d084c41360531ab6
DevFerreiraG <Contato.FerreiraG@outlook.com>
2025-04-20 21:00:05 -0300

    fix: remove logs e garante compatibilidade total com FreeCodeCamp na rota /now

--- myApp.js
+++ myApp.js
@@ -2,32 +2,22 @@ require('dotenv').config();
 let express = require('express');
 let app = express();
 
-console.log('[BOOT] Aplicação Express inicializada');
-
 app.use(function(req, res, next) {
-  console.log(`[GLOBAL MIDDLEWARE] ${req.method} ${req.path} - IP: ${req.ip}`);
   next();
 });
 
 app.use('/public', express.static(__dirname + '/public'));
 
-// app.get('/', function(req, res) {
-//   res.send('Hello Express');
-// });
-
 app.get('/', function(req, res) {
-  console.log('[ROUTE] GET / - Enviando index.html');
   res.sendFile(__dirname + '/views/index.html');
 });
 
 app.get('/json', function(req, res) {
   let message = 'Hello json';
-  console.log('[ROUTE] GET /json - MESSAGE_STYLE:', process.env.MESSAGE_STYLE);
   if (process.env.MESSAGE_STYLE === 'uppercase') {
     message = message.toUpperCase();
   }
   res.json({ message });
-  console.log('[ROUTE] GET /json - Resposta enviada:', { message });
 });
 
 app.get('/now', function(req, res, next) {
@@ -37,9 +27,7 @@ app.get('/now', function(req, res, next) {
   res.json({ time: req.time });
 });
 
-// Middleware de tratamento de erro
 app.use(function(err, req, res, next) {
-  console.error('[ERROR] Middleware de erro capturado:', err);
   res.status(500).json({ error: 'Erro interno do servidor', details: err.message });
 });
 
